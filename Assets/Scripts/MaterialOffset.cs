2659bdab4d40e05b973e84e2725309b888a8adf9
Tarmo Jussila <tarmo.jussila@zaibatsu.fi>
2021-01-30 03:32:12 +0200

    Background tests.

---
+++ Assets/Scripts/MaterialOffset.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialOffset : MonoBehaviour
+{
+    [SerializeField] private Material material;
+    [SerializeField] private float horizontalSpeed;
+    [SerializeField] private float verticalSpeed;
+
+    private void OnValidate()
+    {
+        UpdateOffset(horizontalSpeed, verticalSpeed);
+    }
+
+    private void Update()
+    {
+        UpdateOffset(horizontalSpeed, verticalSpeed);
+    }
+
+    private void UpdateOffset(float horizontalSpeed, float verticalSpeed)
+    {
+        var offset = new Vector2(horizontalSpeed, verticalSpeed);
+
+        material.mainTextureOffset = offset;
+    }
+}
