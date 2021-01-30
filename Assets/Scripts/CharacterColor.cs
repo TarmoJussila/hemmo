fa2968eef52b0603049f5c358efce96a88288e37
Tarmo Jussila <tarmo.jussila@zaibatsu.fi>
2021-01-30 14:39:17 +0200

    Initial character.

---
+++ Assets/Scripts/CharacterColor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CharacterColor : MonoBehaviour
+{
+    [SerializeField] private Color color = Color.white;
+
+    private void OnValidate()
+    {
+        var sprites = GetComponentsInChildren<SpriteRenderer>();
+
+        for (int i = 0; i < sprites.Length; i++)
+        {
+            sprites[i].color = color;
+        }
+    }
+}
