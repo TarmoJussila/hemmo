164befc23f7c3340fc6a6de04d0556b6bbbbc387
Tarmo Jussila <tarmo.jussila@zaibatsu.fi>
2021-01-30 21:54:06 +0200

    Character reach and stand separated. Jumping logic initial.

--- Assets/Scripts/CharacterInput.cs
+++ Assets/Scripts/CharacterInput.cs
@@ -1,9 +1,26 @@
 ﻿using UnityEngine;
 using UnityEngine.InputSystem;
 
+public enum CharacterState
+{
+    Standing = 0,
+    Reaching = 1,
+    Jumping = 2,
+}
+
 public class CharacterInput : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpSpeed;
+    [SerializeField] private Animator animator;
+    [SerializeField] private new Rigidbody2D rigidbody;
+
+    private CharacterState currentState;
+
+    private void Start()
+    {
+        currentState = CharacterState.Standing;
+    }
 
     private void Update()
     {
@@ -21,20 +38,43 @@ public class CharacterInput : MonoBehaviour
 
         if (keyboard.rightArrowKey.isPressed)
         {
-            transform.position += new Vector3(1f * moveSpeed * Time.deltaTime, 0f);
+            //transform.position += new Vector3(1f * moveSpeed * Time.deltaTime, 0f);
+            rigidbody.AddForce(new Vector2(1f * moveSpeed * Time.deltaTime, 0f), ForceMode2D.Force);
         }
         if (keyboard.leftArrowKey.isPressed)
         {
-            transform.position += new Vector3(-1f * moveSpeed * Time.deltaTime, 0f);
+            //transform.position += new Vector3(-1f * moveSpeed * Time.deltaTime, 0f);
+            rigidbody.AddForce(new Vector2(-1f * moveSpeed * Time.deltaTime, 0f), ForceMode2D.Force);
         }
-        if (keyboard.upArrowKey.isPressed)
+
+        if (keyboard.upArrowKey.wasPressedThisFrame)
         {
-            transform.position += new Vector3(0f, 1f * moveSpeed * Time.deltaTime);
+            animator.ResetTrigger("Stand");
+            animator.SetTrigger("Reach");
+            //transform.position += new Vector3(0f, 1f * moveSpeed * Time.deltaTime);
         }
-        if (keyboard.downArrowKey.isPressed)
+        else if (keyboard.downArrowKey.wasPressedThisFrame)
+        {
+            animator.ResetTrigger("Reach");
+            animator.SetTrigger("Stand");
+            //transform.position += new Vector3(0f, -1f * moveSpeed * Time.deltaTime);
+        }
+
+        if (keyboard.spaceKey.wasPressedThisFrame)
+        {
+            rigidbody.AddForce(new Vector2(0f, jumpSpeed * Time.deltaTime), ForceMode2D.Impulse);
+        }
+
+        /*else if (keyboard.upArrowKey.wasReleasedThisFrame)
+        {
+            Debug.Log("was released!");
+            animator.SetTrigger("Stand");
+        }*/
+
+        /*if (keyboard.downArrowKey.isPressed)
         {
             transform.position += new Vector3(0f, -1f * moveSpeed * Time.deltaTime);
-        }
+        }*/
     }
 
     private void UpdateGamepadInput()
