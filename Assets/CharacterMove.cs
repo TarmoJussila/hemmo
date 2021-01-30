f1b2e9e2681ebd1e191a13f780a83df94ad105cd
Tarmo Jussila <tarmo.jussila@zaibatsu.fi>
2021-01-30 22:07:17 +0200

    Character movement wip.

---
+++ Assets/CharacterMove.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour
+{
+    [SerializeField] private CharacterInput characterInput;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpSpeed;
+    [SerializeField] private Animator animator;
+    [SerializeField] private new Rigidbody2D rigidbody;
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
--- Assets/Scripts/CharacterInput.cs
+++ Assets/Scripts/CharacterInput.cs
@@ -4,22 +4,29 @@ using UnityEngine.InputSystem;
 public enum CharacterState
 {
     Standing = 0,
-    Reaching = 1,
-    Jumping = 2,
+    Sitting = 1,
+    Reaching = 2,
+    Jumping = 3,
+    MovingLeft = 4,
+    MovingRight = 5,
+}
+
+public enum CharacterMoveState
+{
+    None = 0,
+    MoveLeft = 1,
+    MoveRight = 2
 }
 
 public class CharacterInput : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed;
-    [SerializeField] private float jumpSpeed;
-    [SerializeField] private Animator animator;
-    [SerializeField] private new Rigidbody2D rigidbody;
-
-    private CharacterState currentState;
+    public CharacterState CurrentState { get; private set; }
+    //public CharacterMoveState CurrentMoveState { get; private set; }
 
     private void Start()
     {
-        currentState = CharacterState.Standing;
+        CurrentState = CharacterState.Standing;
+        //CurrentMoveState = CharacterMoveState.None;
     }
 
     private void Update()
@@ -38,31 +45,37 @@ public class CharacterInput : MonoBehaviour
 
         if (keyboard.rightArrowKey.isPressed)
         {
+            CurrentState = CharacterState.MovingRight;
             //transform.position += new Vector3(1f * moveSpeed * Time.deltaTime, 0f);
-            rigidbody.AddForce(new Vector2(1f * moveSpeed * Time.deltaTime, 0f), ForceMode2D.Force);
+            //GetComponent<Rigidbody>().AddForce(new Vector2(1f * moveSpeed * Time.deltaTime, 0f), ForceMode2D.Force);
         }
-        if (keyboard.leftArrowKey.isPressed)
+        else if (keyboard.leftArrowKey.isPressed)
         {
+            CurrentState = CharacterState.MovingLeft;
             //transform.position += new Vector3(-1f * moveSpeed * Time.deltaTime, 0f);
-            rigidbody.AddForce(new Vector2(-1f * moveSpeed * Time.deltaTime, 0f), ForceMode2D.Force);
-        }
-
-        if (keyboard.upArrowKey.wasPressedThisFrame)
-        {
-            animator.ResetTrigger("Stand");
-            animator.SetTrigger("Reach");
-            //transform.position += new Vector3(0f, 1f * moveSpeed * Time.deltaTime);
-        }
-        else if (keyboard.downArrowKey.wasPressedThisFrame)
-        {
-            animator.ResetTrigger("Reach");
-            animator.SetTrigger("Stand");
-            //transform.position += new Vector3(0f, -1f * moveSpeed * Time.deltaTime);
+            //GetComponent<Rigidbody>().AddForce(new Vector2(-1f * moveSpeed * Time.deltaTime, 0f), ForceMode2D.Force);
         }
 
         if (keyboard.spaceKey.wasPressedThisFrame)
         {
-            rigidbody.AddForce(new Vector2(0f, jumpSpeed * Time.deltaTime), ForceMode2D.Impulse);
+            CurrentState = CharacterState.Jumping;
+            //GetComponent<Rigidbody>().AddForce(new Vector2(0f, jumpSpeed * Time.deltaTime), ForceMode2D.Impulse);
+            return;
+        }
+
+        if (keyboard.upArrowKey.wasPressedThisFrame)
+        {
+            CurrentState = CharacterState.Reaching;
+            //animator.ResetTrigger("Stand");
+            //animator.SetTrigger("Reach");
+            //transform.position += new Vector3(0f, 1f * moveSpeed * Time.deltaTime);
+        }
+        else if (keyboard.downArrowKey.wasPressedThisFrame)
+        {
+            CurrentState = CharacterState.Standing;
+            //animator.ResetTrigger("Reach");
+            //animator.SetTrigger("Stand");
+            //transform.position += new Vector3(0f, -1f * moveSpeed * Time.deltaTime);
         }
 
         /*else if (keyboard.upArrowKey.wasReleasedThisFrame)
