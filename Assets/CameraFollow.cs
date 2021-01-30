10d6d3790b9d69783b1ae2c21db71903f492098d
Tarmo Jussila <tarmo.jussila@zaibatsu.fi>
2021-01-31 00:13:39 +0200

    Character camera follow.

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform followTransform;
+    [SerializeField] private float smoothTime;
+    [SerializeField] private Vector2 followOffset;
+
+    private Vector3 currentVelocity = Vector3.zero;
+
+    private void FixedUpdate()
+    {
+        var currentPosition = Camera.main.transform.position;
+        var targetPosition = new Vector3(followTransform.position.x + followOffset.x, followTransform.position.y + followOffset.y, Camera.main.transform.position.z);
+
+        Camera.main.transform.position = Vector3.SmoothDamp(currentPosition, targetPosition, ref currentVelocity, smoothTime * Time.deltaTime);
+    }
+}
