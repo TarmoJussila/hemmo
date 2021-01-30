654199e798719bc35cfd3610f65f0d246a867286
Tarmo Jussila <tarmo.jussila@zaibatsu.fi>
2021-01-30 23:07:00 +0200

    Character state events.

--- Assets/CharacterMove.cs
+++ Assets/CharacterMove.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CharacterMove : MonoBehaviour
 {
@@ -9,9 +7,87 @@ public class CharacterMove : MonoBehaviour
     [SerializeField] private float jumpSpeed;
     [SerializeField] private Animator animator;
     [SerializeField] private new Rigidbody2D rigidbody;
+    [SerializeField] private LayerMask worldLayerMask;
+    [SerializeField] private float jumpTimeLimit = 0.1f;
+
+    private float jumpTimer = 0f;
+
+    private void OnEnable()
+    {
+        CharacterInput.OnCharacterJump += OnCharacterJump;
+        CharacterInput.OnCharacterReach += OnCharacterReach;
+        CharacterInput.OnCharacterSit += OnCharacterSit;
+    }
+
+    private void OnDisable()
+    {
+        CharacterInput.OnCharacterJump -= OnCharacterJump;
+        CharacterInput.OnCharacterReach -= OnCharacterReach;
+        CharacterInput.OnCharacterSit -= OnCharacterSit;
+    }
 
     private void FixedUpdate()
     {
-        
+        if (characterInput.IsMovingRight)
+        {
+            rigidbody.AddForce(new Vector2(1f * moveSpeed * Time.deltaTime, 0f), ForceMode2D.Force);
+        }
+        else if (characterInput.IsMovingLeft)
+        {
+            rigidbody.AddForce(new Vector2(-1f * moveSpeed * Time.deltaTime, 0f), ForceMode2D.Force);
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (jumpTimer > 0f)
+        {
+            jumpTimer -= Time.deltaTime;
+        }
+    }
+
+    private void OnCharacterJump()
+    {
+        if (!CanJump() || !IsTouchingGround())
+        {
+            return;
+        }
+
+        animator.ResetTrigger("Reach");
+        animator.SetTrigger("Stand");
+        rigidbody.AddForce(new Vector2(0f, jumpSpeed * Time.deltaTime), ForceMode2D.Impulse);
+        jumpTimer = jumpTimeLimit;
+    }
+
+    private void OnCharacterReach()
+    {
+        if (!IsTouchingGround())
+        {
+            return;
+        }
+
+        animator.ResetTrigger("Stand");
+        animator.SetTrigger("Reach");
+    }
+
+    private void OnCharacterSit()
+    {
+        if (!IsTouchingGround())
+        {
+            return;
+        }
+
+        animator.ResetTrigger("Reach");
+        animator.SetTrigger("Stand");
+    }
+
+    private bool IsTouchingGround()
+    {
+        return rigidbody.IsTouchingLayers(worldLayerMask);
+    }
+
+    private bool CanJump()
+    {
+        return jumpTimer <= 0f;
     }
 }
--- Assets/Scripts/CharacterInput.cs
+++ Assets/Scripts/CharacterInput.cs
@@ -1,32 +1,20 @@
 ﻿using UnityEngine;
 using UnityEngine.InputSystem;
 
-public enum CharacterState
-{
-    Standing = 0,
-    Sitting = 1,
-    Reaching = 2,
-    Jumping = 3,
-    MovingLeft = 4,
-    MovingRight = 5,
-}
-
-public enum CharacterMoveState
-{
-    None = 0,
-    MoveLeft = 1,
-    MoveRight = 2
-}
-
 public class CharacterInput : MonoBehaviour
 {
-    public CharacterState CurrentState { get; private set; }
-    //public CharacterMoveState CurrentMoveState { get; private set; }
+    public delegate void CharacterEvent();
+    public static CharacterEvent OnCharacterJump;
+    public static CharacterEvent OnCharacterReach;
+    public static CharacterEvent OnCharacterSit;
+
+    public bool IsMovingLeft { get; private set; }
+    public bool IsMovingRight { get; private set; }
 
     private void Start()
     {
-        CurrentState = CharacterState.Standing;
-        //CurrentMoveState = CharacterMoveState.None;
+        IsMovingLeft = false;
+        IsMovingRight = false;
     }
 
     private void Update()
@@ -45,49 +33,30 @@ public class CharacterInput : MonoBehaviour
 
         if (keyboard.rightArrowKey.isPressed)
         {
-            CurrentState = CharacterState.MovingRight;
-            //transform.position += new Vector3(1f * moveSpeed * Time.deltaTime, 0f);
-            //GetComponent<Rigidbody>().AddForce(new Vector2(1f * moveSpeed * Time.deltaTime, 0f), ForceMode2D.Force);
+            IsMovingRight = true;
         }
         else if (keyboard.leftArrowKey.isPressed)
         {
-            CurrentState = CharacterState.MovingLeft;
-            //transform.position += new Vector3(-1f * moveSpeed * Time.deltaTime, 0f);
-            //GetComponent<Rigidbody>().AddForce(new Vector2(-1f * moveSpeed * Time.deltaTime, 0f), ForceMode2D.Force);
+            IsMovingLeft = true;
+        }
+        else
+        {
+            IsMovingRight = false;
+            IsMovingLeft = false;
         }
 
         if (keyboard.spaceKey.wasPressedThisFrame)
         {
-            CurrentState = CharacterState.Jumping;
-            //GetComponent<Rigidbody>().AddForce(new Vector2(0f, jumpSpeed * Time.deltaTime), ForceMode2D.Impulse);
-            return;
+            OnCharacterJump?.Invoke();
         }
-
-        if (keyboard.upArrowKey.wasPressedThisFrame)
+        else if (keyboard.upArrowKey.wasPressedThisFrame)
         {
-            CurrentState = CharacterState.Reaching;
-            //animator.ResetTrigger("Stand");
-            //animator.SetTrigger("Reach");
-            //transform.position += new Vector3(0f, 1f * moveSpeed * Time.deltaTime);
+            OnCharacterReach?.Invoke();
         }
         else if (keyboard.downArrowKey.wasPressedThisFrame)
         {
-            CurrentState = CharacterState.Standing;
-            //animator.ResetTrigger("Reach");
-            //animator.SetTrigger("Stand");
-            //transform.position += new Vector3(0f, -1f * moveSpeed * Time.deltaTime);
+            OnCharacterSit?.Invoke();
         }
-
-        /*else if (keyboard.upArrowKey.wasReleasedThisFrame)
-        {
-            Debug.Log("was released!");
-            animator.SetTrigger("Stand");
-        }*/
-
-        /*if (keyboard.downArrowKey.isPressed)
-        {
-            transform.position += new Vector3(0f, -1f * moveSpeed * Time.deltaTime);
-        }*/
     }
 
     private void UpdateGamepadInput()
