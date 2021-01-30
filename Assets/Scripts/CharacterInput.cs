3ca6a9d75a38cd14c5a083c88908044ed008f8dc
Tarmo Jussila <tarmo.jussila@zaibatsu.fi>
2021-01-30 05:10:20 +0200

    Enabled new input system. Initial character that moves (wip).

---
+++ Assets/Scripts/CharacterInput.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CharacterInput : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+
+    private void Update()
+    {
+        UpdateKeyboardInput();
+        UpdateGamepadInput();
+    }
+
+    private void UpdateKeyboardInput()
+    {
+        var keyboard = Keyboard.current;
+        if (keyboard == null)
+        {
+            return;
+        }
+
+        if (keyboard.rightArrowKey.isPressed)
+        {
+            transform.position += new Vector3(1f * moveSpeed * Time.deltaTime, 0f);
+        }
+        if (keyboard.leftArrowKey.isPressed)
+        {
+            transform.position += new Vector3(-1f * moveSpeed * Time.deltaTime, 0f);
+        }
+        if (keyboard.upArrowKey.isPressed)
+        {
+            transform.position += new Vector3(0f, 1f * moveSpeed * Time.deltaTime);
+        }
+        if (keyboard.downArrowKey.isPressed)
+        {
+            transform.position += new Vector3(0f, -1f * moveSpeed * Time.deltaTime);
+        }
+    }
+
+    private void UpdateGamepadInput()
+    {
+        var gamepad = Gamepad.current;
+        if (gamepad == null)
+        {
+            return;
+        }
+    }
+}
